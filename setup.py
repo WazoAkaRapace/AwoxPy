cde817400148cb8b7ab510bbf1530b5800e730d8
Rapace21 <rapace21@gmail.com>
2017-11-04 18:19:36 +0100

    [WIP] Adding bluepy to needed dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
+
 setup(
-  name = 'awoxpy',
-  packages = ['awoxpy'],
-  version = '0.6',
-  description = 'A library that help you control Awox Smartlight with BTLE protocol',
-  author = 'Mika Benoit',
-  author_email = 'mika.benoit@gmail.com',
-  url = 'https://github.com/Rapace21/AwoxPy',
-  download_url = 'https://github.com/Rapace21/AwoxPy/archive/0.6.tar.gz',
-  keywords = ['awox', 'bluetooth', 'lib'],
-  classifiers = [],
-  include_package_data=True,
-  zip_safe=False,
+    name='awoxpy',
+    packages=['awoxpy'],
+    version='0.7',
+    description='A library that help you control Awox Smartlight with BTLE protocol',
+    author='Mika Benoit',
+    author_email='mika.benoit@gmail.com',
+    url='https://github.com/Rapace21/AwoxPy',
+    download_url='https://github.com/Rapace21/AwoxPy/archive/0.7.tar.gz',
+    keywords=['awox', 'bluetooth', 'lib'],
+    classifiers=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'bluepy',
+    ],
 )
+
