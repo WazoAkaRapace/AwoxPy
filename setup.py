9d2feab97fb7118675eb9a614d27b4ea2ba2e510
Rapace21 <rapace21@gmail.com>
2017-11-04 17:19:56 +0100

    [WIP] Moving on 0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'awoxpy',
   packages = ['awoxpy'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.4',
   description = 'A library that help you control Awox Smartlight with BTLE protocol',
   author = 'Mika Benoit',
   author_email = 'mika.benoit@gmail.com',
   url = 'https://github.com/Rapace21/AwoxPy', # use the URL to the github repo
-  download_url = 'https://github.com/Rapace21/AwoxPy/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/Rapace21/AwoxPy/archive/0.4.tar.gz', # I'll explain this in a second
   keywords = ['awox', 'bluetooth', 'lib'], # arbitrary keywords
   classifiers = [],
 )
