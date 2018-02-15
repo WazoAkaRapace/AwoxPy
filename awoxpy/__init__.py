e5d8d19f0e74924e04b0cad2ddbd933c9da32b85
Rapace21 <rapace21@gmail.com>
2018-02-15 19:39:27 +0100

    [WIP] Doing some variable typing...

--- awoxpy/__init__.py
+++ awoxpy/__init__.py
@@ -3,7 +3,7 @@
 #
 # This code is released under the terms of the Apache 2.0 License. See the LICENSE
 # file for more details.
-
+import struct
 import time
 
 from bluepy import btle
@@ -12,6 +12,7 @@ from bluepy import btle
 class awoxAroma:
     def __init__(self, mac):
         self.mac = mac
+        self.color_mode = False
         self.set_state(0, 0, 0, 0, 0, 0, False)
 
     def set_state(self, white, whiteBrightness, red, green, blue, colourBrightness, power):
@@ -27,7 +28,8 @@ class awoxAroma:
         try:
             try:
                 self.device = btle.Peripheral(self.mac, addrType=btle.ADDR_TYPE_PUBLIC)
-            except:
+            except btle.BTLEException as inst:
+                print(inst)
                 btle.Scanner().scan(10)
                 self.device = btle.Peripheral(self.mac, addrType=btle.ADDR_TYPE_PUBLIC)
 
@@ -38,7 +40,7 @@ class awoxAroma:
                 if handle.uuid == "74532143-fff1-460d-8e8a-370f934d40be":
                     self.rgbhandle = handle
                 if handle.uuid == "5b430c99-cb06-4c66-be2c-b538acfd1961":
-                    self.whitehandle = handle #From 0x00 to 0x7F
+                    self.whitehandle = handle  # From 0x00 to 0x7F
                 if handle.uuid == "1c537b0a-4eaa-4e19-b98c-eaaa5bcd9bc9":
                     self.rgbBrightnessHandle = handle  # From 0x00 to 0x64
                 if handle.uuid == "d8da934c-3d8f-4bdf-9230-f61295b69570":
@@ -48,7 +50,8 @@ class awoxAroma:
 
             self.get_state()
             return True
-        except:
+        except btle.BTLEException as inst:
+            print(inst)
             return False
 
     def send_packet(self, handle, data):
@@ -82,6 +85,7 @@ class awoxAroma:
         self.send_packet(self.statehandle, packet)
 
     def set_rgb(self, red, green, blue):
+        self.color_mode = True
         self.red = red
         self.green = green
         self.blue = blue
@@ -89,6 +93,7 @@ class awoxAroma:
         self.send_packet(self.rgbhandle, packet)
 
     def set_white(self, white):
+        self.color_mode = False
         self.white = white
         if type(white) == bytes:
             packet = [white]
@@ -97,11 +102,13 @@ class awoxAroma:
         self.send_packet(self.whitehandle, packet)
 
     def set_white_brightness(self, brightness):
+        self.color_mode = False
         self.whiteBrightness = brightness
         packet = bytearray([brightness])
         self.send_packet(self.whiteBrightnessHandle, packet)
 
     def set_colour_brightness(self, brightness):
+        self.color_mode = True
         self.colourBrightness = brightness
         packet = bytearray([brightness])
         self.send_packet(self.rgbBrightnessHandle, packet)
@@ -114,18 +121,18 @@ class awoxAroma:
             self.power = True
 
         readed = self.read(self.rgbhandle)
-        self.red = readed[0]
-        self.green = readed[1]
-        self.blue = readed[2]
+        self.red = struct.unpack("h", readed[0] + "\x00")[0]
+        self.green = struct.unpack("h", readed[1] + "\x00")[0]
+        self.blue = struct.unpack("h", readed[2] + "\x00")[0]
 
         readed = self.read(self.rgbBrightnessHandle)
-        self.colourBrightness = readed
+        self.colourBrightness = struct.unpack("h", readed[0] + "\x00")[0]
 
         readed = self.read(self.whitehandle)
-        self.white = readed
+        self.white = struct.unpack("h", readed + "\x00")[0]
 
         readed = self.read(self.whiteBrightnessHandle)
-        self.whiteBrightness = readed
+        self.whiteBrightness = struct.unpack("h", readed[0] + "\x00")[0]
 
     def get_on(self):
         return self.power
@@ -141,7 +148,7 @@ class awoxAroma:
             return ord(self.colourBrightness)
 
     def get_white(self):
-            return self.white
+        return self.white
 
     def get_white_brightness(self):
         if type(self.whiteBrightness) == int:
@@ -149,3 +156,14 @@ class awoxAroma:
         else:
             return ord(self.whiteBrightness)
 
+    def get_brightness(self):
+        if self.color_mode:
+            return self.get_colour_brightness()
+        else:
+            return self.get_white_brightness()
+
+    def set_brightness(self, brightness):
+        if self.color_mode:
+            self.set_colour_brightness(brightness)
+        else:
+            self.set_white_brightness(brightness)
